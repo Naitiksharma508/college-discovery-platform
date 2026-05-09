63c61e755f1c8bfd933e1fce8e7aedafd74f35d6
Naitik Sharma <naitiksharma2790@gmail.com>
2026-05-09 16:43:14 +0530

    Fix case sensitivity for Home import

--- college-frontend/src/App.jsx
+++ college-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './home';
 import CollegeDetail from './CollegeDetail';
 import Compare from './Compare';
 
