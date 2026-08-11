df78450a81905f69dc623537f9c0af2b3f12290f
Naitik Sharma <naitiksharma2790@gmail.com>
2026-08-11 22:49:17 +0530

    Fix Cors error

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 5000;
 
-// Middleware
+const port = process.env.PORT || 5000;
+app.use(cors({
+    origin: '*' // This allows your Vercel site (and anyone else) to fetch data
+}));
 app.use(cors());
 app.use(express.json());
-
 // PostgreSQL Connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
